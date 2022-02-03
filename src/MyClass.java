7fa0864c89374d8fc8c9d3958dc02fe29ddb1d12
vonsouza <souzavon@gmail.com>
2022-02-03 20:23:06 -0300

    Initial commit

---
+++ src/MyClass.java
@@ -0,0 +1,14 @@
+/*
+* Software Design and Development :: CS 246
+* 04 Prove
+* Version Control with GitHub
+* @author: Von Souza
+* 02/03/2022
+*/
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Von");
+    }
+    
+}
